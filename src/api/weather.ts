02b6f5e3cd52e8ec84e992325361bf5807ba833e
Ayuu13 <ayuandi517@gmail.com>
2025-10-29 14:29:39 +0700

    second commit - day 2

---
+++ src/api/weather.ts
@@ -0,0 +1,19 @@
+export async function fetchWeather (city:string):
+    Promise<{city:string; temperature:number}>{
+        return new Promise((resolve)=>{
+            setTimeout(()=>{
+                resolve({
+                    city,
+                    temperature:Math.floor(Math.random()*30)+10  // asumsi random nya dari 10-40
+                });
+            },1000)
+        })
+}
+
+// function sederhana tanpa promise
+// function weather (city){
+//     return ({
+//         city,
+//         temperature: Math.floor(Math.random()*30)+10
+//     })
+// }
